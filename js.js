c0be1e90e7e3b79e70c1bc2a0fec5f3a75c82087
KaterynaLutska <ket.lutska@gmail.com>
2020-11-20 10:29:41 +0200

    hw-08-fixed

--- js.js
+++ js.js
@@ -1,9 +1,11 @@
 import galleryList from './gallery-items.js'; 
 
-const gallery = document.querySelector('.js-gallery');
-const modalImages = document.querySelector('.lightbox__image')
-const modal = document.querySelector('.lightbox')
-const modalBtn = document.querySelector('.lightbox__button')
+const refs = {
+    gallery: document.querySelector('.js-gallery'),
+    modalImages: document.querySelector('.lightbox__image'),
+    modal: document.querySelector('.lightbox'),
+    modalBtn: document.querySelector('.lightbox__button'),
+}
 
 
 function createImageItem(array) {
@@ -18,7 +20,7 @@ function createImageItem(array) {
             
             const ref = document.createElement('a');
             ref.classList.add('gallery__link');
-            //ref.setAttribute('href', array[i].original);
+            ref.setAttribute('href', array[i].original);
             ref.appendChild(img);
 
             const item = document.createElement('li');
@@ -27,40 +29,31 @@ function createImageItem(array) {
             
             items.push(item);
         }   
-return items
+    return items
 }
 
 const imageItems = createImageItem(galleryList);
-gallery.append(...imageItems)
 
-const images = document.querySelectorAll('.gallery__image'); // ось цю переміну не можу підняти вгору, все ламаєтьсяж 
-
-gallery.addEventListener('click', onClick)
+refs.gallery.append(...imageItems)
+refs.gallery.addEventListener('click', onClick)
+refs.modalBtn.addEventListener('click', modalClose)
 
 function onClick(event) {
+    event.preventDefault();
     if (event.target.nodeName !== 'IMG') {
         return
-      }
-    const aktive = event.target;
-    const url = aktive.dataset.source;
-    return  url
+    }
+    refs.modal.classList.add('is-open')
+    refs.modalImages.src = event.target.dataset.source
 }
 
 
-for (const image of images) {
-    image.addEventListener('click', () => {
-        modal.classList.add('is-open')
-          modalImages.src = image.dataset.source   
-    })
-    modalBtn.addEventListener('click', () => {
-        modal.classList.remove('is-open')
-        modalImages.src = '';
-    })
+function modalClose(close) {
+    close.preventDefault();
+    if (close.target.nodeName !== 'BUTTON') {
+        return
+    }
+        refs.modal.classList.remove('is-open')
+        refs.modalImages.src = '';
 }
-
-
-
-
-
-
-
+    
