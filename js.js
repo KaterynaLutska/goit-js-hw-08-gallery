3a4311cec9c5ed4eaacd8418071355e9fe11acff
KaterynaLutska <ket.lutska@gmail.com>
2020-11-20 09:44:15 +0200

    hw-08

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
 import galleryList from './gallery-items.js'; 
-// console.log(galleryList);
+
 const gallery = document.querySelector('.js-gallery');
 const modalImages = document.querySelector('.lightbox__image')
+const modal = document.querySelector('.lightbox')
+const modalBtn = document.querySelector('.lightbox__button')
 
 
 function createImageItem(array) {
@@ -9,37 +11,29 @@ function createImageItem(array) {
         for (let i = 0; i < array.length; i++) {
             
             const img = document.createElement('img');
-            img.classList.add('gallery__image')
-            img.setAttribute('src', array[i].preview)
-            img.setAttribute('data-source', array[i].original)
-            img.setAttribute('alt', array[i].description)
-            // console.dir(img);
+            img.classList.add('gallery__image');
+            img.setAttribute('src', array[i].preview);
+            img.setAttribute('data-source', array[i].original);
+            img.setAttribute('alt', array[i].description);
             
             const ref = document.createElement('a');
-            ref.classList.add('gallery__link')
-            //ref.setAttribute('href', array[i].original)
-            ref.appendChild(img)
-            // console.log(ref);
+            ref.classList.add('gallery__link');
+            //ref.setAttribute('href', array[i].original);
+            ref.appendChild(img);
 
             const item = document.createElement('li');
-            item.classList.add('gallery__item')
-            item.appendChild(ref)
-            // console.log(item); 
+            item.classList.add('gallery__item');
+            item.appendChild(ref);
             
-            items.push(item)
+            items.push(item);
         }   
 return items
 }
 
-
 const imageItems = createImageItem(galleryList);
 gallery.append(...imageItems)
-console.log(gallery); // вся вкладеність з ul 
 
-
-const images = document.querySelectorAll('.gallery__image');
-console.dir(images);
-
+const images = document.querySelectorAll('.gallery__image'); // ось цю переміну не можу підняти вгору, все ламаєтьсяж 
 
 gallery.addEventListener('click', onClick)
 
@@ -48,27 +42,25 @@ function onClick(event) {
         return
       }
     const aktive = event.target;
-    const url = aktive.dataset.source
-    console.log(url);
+    const url = aktive.dataset.source;
     return  url
 }
 
-//Відкриття модального вікна при натисканні на елементі галереї.
-
-const modal = document.querySelector('.lightbox')
-const modalBtn = document.querySelector('.lightbox__button')
 
 for (const image of images) {
-
     image.addEventListener('click', () => {
-        console.log('click');
         modal.classList.add('is-open')
           modalImages.src = image.dataset.source   
     })
-
     modalBtn.addEventListener('click', () => {
         modal.classList.remove('is-open')
         modalImages.src = '';
-    }) 
+    })
 }
+
+
+
+
+
+
 
