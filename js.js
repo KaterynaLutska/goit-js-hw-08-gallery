02d10a5b4555f1f587f225b335b1525a72f3bac6
KaterynaLutska <ket.lutska@gmail.com>
2020-11-22 22:05:58 +0200

    fixed hw

--- js.js
+++ js.js
@@ -1,5 +1,3 @@
-
-
 import galleryList from './gallery-items.js';
 
 const refs = {
@@ -8,42 +6,39 @@ const refs = {
     modal: document.querySelector('.lightbox'),
     modalBtn: document.querySelector('.lightbox__button'),
     overlay: document.querySelector('.lightbox__overlay'),
+    buttonRight: document.querySelector('.right'),
+    buttonLeft: document.querySelector('.left')
 }
 
+const items = ((element) => {
+    const img = document.createElement('img');
+    img.setAttribute('src', element.preview);
+    img.setAttribute('data-source', element.original);
+    img.setAttribute('data-index', element.index);
+    img.setAttribute('alt', element.description);
+    img.classList.add('gallery__image')
 
-function createImageItem(array) {
-    const items = [];
-        for (let i = 0; i < array.length; i++) {
-            
-            const img = document.createElement('img');
-            img.classList.add('gallery__image');
-            img.setAttribute('src', array[i].preview);
-            img.setAttribute('data-source', array[i].original);
-            img.setAttribute('data-index', array[i].index);
-            img.setAttribute('alt', array[i].description);
-            
-            const ref = document.createElement('a');
-            ref.classList.add('gallery__link');
-            ref.setAttribute('href', array[i].original);
-            ref.appendChild(img);
+    const ref = document.createElement('a');
+    ref.classList.add('gallery__link');
+    ref.setAttribute('href', element.original);
+    ref.appendChild(img);  
 
-            const item = document.createElement('li');
-            item.classList.add('gallery__item');
-            item.appendChild(ref);
-            
-            items.push(item);
-        }   
-    return items
-}
+    const item = document.createElement('li');
+    item.classList.add('gallery__item');
+    item.appendChild(ref);
+    return item
+})
 
+const imageItems = galleryList.map((element) => items(element))
 
-const imageItems = createImageItem(galleryList);
-refs.gallery.append(...imageItems);
+refs.gallery.append(...imageItems)
 
 
 refs.gallery.addEventListener('click', oppenModal);
 refs.modalBtn.addEventListener('click', modalClose);
 refs.overlay.addEventListener('click', onBeckDropCkick);
+refs.buttonLeft.addEventListener('click', switchToNextImage)
+refs.buttonRight.addEventListener('click', switchToPrevImage)
 
 function onKeybordPress(event) {
     if (event.code === 'Escape') {
@@ -63,16 +58,19 @@ function oppenModal(event) {
         return
     }
     window.addEventListener('keydown', onKeybordPress);
-    window.addEventListener('keydown', switchToNextImage)
-    window.addEventListener('keydown', switchToPrevImage)
-    
+    window.addEventListener('keydown', switchToNextImage);
+    window.addEventListener('keydown', switchToPrevImage);
+
     refs.modal.classList.add('is-open');
     refs.activeImage.src = event.target.dataset.source;
-    refs.activeImage.dataset.index = event.target.dataset.index   
+    refs.activeImage.dataset.index = event.target.dataset.index;   
 }
 
 function modalClose() {
     window.removeEventListener('keydown', onKeybordPress);
+    window.removeEventListener('keydown', switchToNextImage);
+    window.removeEventListener('keydown', switchToPrevImage);
+
 
     refs.modal.classList.remove('is-open');
     refs.activeImage.src = '';  
@@ -86,21 +84,20 @@ function onBeckDropCkick(event) {
 
 function switchToNextImage () {
     let imageIndex = +refs.activeImage.dataset.index
-    console.log(imageIndex);
-
+   
     if (imageIndex === galleryList.length - 1) {
         refs.activeImage.src = galleryList[0].original;
         refs.activeImage.dataset.index = 0;
     }
     else {
         refs.activeImage.src = galleryList[imageIndex + 1].original
-        console.log(refs.activeImage.src);
         refs.activeImage.dataset.index = imageIndex + 1
     }
 }
+
 function switchToPrevImage() {
     let imageIndex = +refs.activeImage.dataset.index
-    console.log(imageIndex);
+  
      if (imageIndex === 0) {
          refs.activeImage.src = galleryList[galleryList.length - 1].original;
          refs.activeImage.dataset.index = galleryList.length - 1;
@@ -110,4 +107,8 @@ function switchToPrevImage() {
         refs.activeImage.dataset.index = imageIndex - 1
     }
 }
-    
+
+
+
+
+
